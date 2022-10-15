15445d54f914ce478c5539baf340a674868c00e9
Heaven Aldrico <4d3.h34v3n@gmail.com>
2022-10-15 22:08:57 +0700

    fix: messy routes

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,6 @@ const routes = [
     component: () => import('@/views/LoginPage.vue')
   },
   {
-    path: '/',
-    name: 'index',
-    component: () => import('../views/IndexPage.vue')
-  },
-  {
-    path: '/login',
-    name: 'login',
-    component: () => import('../views/LoginPage.vue')
-  },
-  {
     path: '/register',
     name: 'register',
     component: () => import('@/views/RegisterPage.vue')
